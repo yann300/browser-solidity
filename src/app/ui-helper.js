5358ab01fed099cf0f71a2539b7ab2b0ef505418
Alex Beregszaszi <alex@rtfs.hu>
2016-09-01 10:19:07 +0100

    Support missing opcodes field

--- src/app/ui-helper.js
+++ src/app/ui-helper.js
@@ -150,7 +150,7 @@ module.exports = {
       details.append(this.tableRow('Runtime Bytecode', contract.runtimeBytecode));
     }
 
-    if (contract.opcodes !== '') {
+    if (contract.opcodes !== undefined && contract.opcodes !== '') {
       details.append(this.tableRow('Opcodes', contract.opcodes));
     }
 
